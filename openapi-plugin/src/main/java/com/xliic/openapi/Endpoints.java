b60f83547786e53c9e42c0e9f5c53d539a0e2fbd
Viacheslav Fateev <viacheslav.fateev@gmail.com>
2025-10-14 15:19:54 +0300

    Set DEV_KDB_URL to KDB_URL

--- openapi-plugin/src/main/java/com/xliic/openapi/Endpoints.java
+++ openapi-plugin/src/main/java/com/xliic/openapi/Endpoints.java
@@ -6,7 +6,7 @@ public class Endpoints {
 
     private static final String DEV_FREEMIUMD_URL = "https://stateless.dev.42crunch.com";
     private static final String DEV_CLI_FREEMIUMD_HOST = "stateless.dev.42crunch.com:443";
-    private static final String DEV_KDB_URL = "https://platform-community.dev.42crunch.com/kdb/audit-with-yaml.json";
+    private static final String DEV_KDB_URL = "https://platform.42crunch.com/kdb/audit-with-yaml.json";
 
     private static final String FREEMIUMD_URL = "https://stateless.42crunch.com";
     private static final String CLI_FREEMIUMD_HOST = "stateless.42crunch.com:443";
